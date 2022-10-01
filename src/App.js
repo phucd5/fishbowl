b0857e4ccbe8d866ab8c4524082f4003b5dc1308
phucd5 <phucduongh@gmail.com>
2022-10-01 14:18:50 -0400

    multiple pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './pages/Home'
+import Upload from './pages/Upload.js'
+import VideoPlayer from './pages/VideoPlayer';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path='/upload' element={<Upload/>} />
+        <Route path ='/video' element={<VideoPlayer />} />
+      </Routes>
+
   );
 }
 
---
+++ src/pages/VideoPlayer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function VideoPlayer() {
+    return (
+        <h1>Upload</h1>
+    )
+}
+
+export default VideoPlayer;

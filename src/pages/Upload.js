d57ecdd4d8db07d243867d2ed9ee7961b894e1f9
“Calvin <Calvinli0824@gmail.com>
2022-10-02 07:00:49 -0400

    upload page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -7,18 +7,19 @@ function Upload() {
     return (
         <div>
             <div class="boxwrapper">
-                <div id="box1">1</div>
+                <div id="box1">
+                </div>
                 <div class="boxwrapper2">
-                <div id="box2">
+                <div id="box2" >
                     <input type="text" placeholder="Title" required></input>
                 </div>
                 <div id="box3">
                 <textarea placeholder='Description of lecture'></textarea>
                 </div>
                 <div className="boxwrapper3">
-                    <a href='/home'><button>Cancel</button></a>
-                    <button>Save</button>
-                    <button>Upload</button>
+                    <a href='/home'><button className='otherButs'>Cancel</button></a>
+                    <button className='otherButs'>Save</button>
+                    <button id='submitBut'>Upload</button>
                 </div>
                 </div>
                 

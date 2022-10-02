1fc659403a7bde550a842e386bde03d5d5036043
“Calvin <Calvinli0824@gmail.com>
2022-10-02 00:34:39 -0400

    added a button and upload page

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { Navigate, useNavigate } from 'react-router-dom';
 
 
 function Upload() {
 
-
-
-
     return (
         <div>
-            Hello
+            <div class="boxwrapper">
+                <div id="box1">1</div>
+                <div class="boxwrapper2">
+                <div id="box2">
+                    <input type="text" placeholder="Title" required></input>
+                </div>
+                <div id="box3">
+                <textarea placeholder='Description of lecture'></textarea>
+                </div>
+                <div className="boxwrapper3">
+                    <a href='/home'><button>Cancel</button></a>
+                    <button>Save</button>
+                    <button>Upload</button>
+                </div>
+                </div>
+                
+            </div>
         </div>
-        );
+    );
 
 }
 
